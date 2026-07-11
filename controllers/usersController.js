ae1737cfe22d4076b32c11ea3ff97163e005c27e
NomadicHippiee <nomadichippie@proton.me>
2026-07-11 22:02:20 +0200

    Fix typos

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 const usersStorage = require("../storages/usersStorage");
-const { body, validationResults, matchedData } = require("express-validator");
+const { body, validationResult, matchedData } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
@@ -7,7 +7,7 @@ const lengthErr = "must be between 1 and 10 characters.";
 const validateUser = [
     body("firstName").trim().isAlpha().withMessage(`First name ${alphaErr}`)
     .isLength({ min: 1, max: 10}).withMessage(`First name ${lengthErr}`),
-    body("lastName").trim().isAlpha(`Last name ${alphaErr}`).isLength({ min: 1, max: 10}).withMessage(`Last name ${lengthErr}`)
+    body("lastName").trim().isAlpha().withMessage(`Last name ${alphaErr}`).isLength({ min: 1, max: 10}).withMessage(`Last name ${lengthErr}`)
 ];
 
 exports.usersListGet = (req, res) => {
@@ -26,9 +26,9 @@ exports.usersCreateGet = (req, res) => {
 
 exports.usersCreatePost = [
     validateUser, (req, res) => {
-        const errors = validationResults(req);
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(404).render("createUser", {
+            return res.status(400).render("createUser", {
                 title: "Create user",
                 errors: errors.array(),
             });
